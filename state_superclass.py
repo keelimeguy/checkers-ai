cfdd402de77e4c5b516a2a71cfb81fb466d7f55c
ofk13001 <oliver.kisielius@uconn.edu>
2017-04-04 22:10:58 -0400

    Add a function to help parse boards

--- state_superclass.py
+++ state_superclass.py
@@ -10,3 +10,35 @@ class CheckersGameState:
     create.
     """
     pass
+
+def parse_board_string(board_rep):
+    """Take a board representation in the canonical format (see unit tests) and
+    return tuple (L, P) where
+
+    L is a list of tuples (row, column, character) with the locations and types
+    (character "W", "w", "B", or "b") of all the checkers
+
+    P is the current player, "White" or "Black"
+    """
+    lines = board_rep.strip().split()  # split on whitespace (e.g. newlines)
+    pieces = []
+    for row, line in enumerate(lines[:8][::-1]):
+        for column, char in enumerate(line.strip()):
+            if char in 'wWbB':
+                if (column % 2) == (row % 2):
+                    pieces.append((row, column, char))
+                else:
+                    raise ValueError(
+                        "Bad location ({}, {}) for piece '{}'".format(
+                            row, column, char))
+            elif char not in "-+":
+                raise ValueError("Unexpected char {} found in board "
+                                 "description at ({}, {})".format(char,
+                                                                  row,
+                                                                  column))
+
+    player = lines[8][:5]
+    if player not in ["White", "Black"]:
+        raise ValueError("Unknown value '{}' for player "
+                         "- expected 'Black' or 'White'".format(player))
+    return (pieces, player)
