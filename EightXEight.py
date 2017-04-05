ae5358247dec56a29b54f503b9e840f1265d69cc
macira <mac.ira353@gmail.com>
2017-04-05 01:31:21 -0400

    What is going on here

--- EightXEight.py
+++ EightXEight.py
@@ -6,19 +6,17 @@ WK = "W"
 BC = "b"
 BK = "B"
 
-
 class EightXEightGameState(CheckersGameState):
-"""Uses a double array to represent the board"""
-
-	def Variables(self)
+	"""Uses a double array to represent the board"""
+	def Variables(self):
 		self.playerBlack = "true"
-		"""rows and columns"""
 		r,c = 8,8;
-		Board = [[ 0 for x  in range(r)] for y in range(c)]
+		Board = [[0 for x in range(r)] for y in range(C)]
+		for r in range(r):
+			for c in range(c):
+				Board[r][c] = "-"
+		print Board
 
-		for r in range(len(board))
-		 	for c in range(len(board))
-		 	 	Board[r][c] = "-" 
 
-		 	 	
+
 
