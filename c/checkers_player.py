098bea6af9eafa7b5de33f63ba485f9e7df8f774
Keelin Becker-Wheeler <keelin.wheeler@gmail.com>
2017-04-15 18:00:38 -0400

    Added game result (win/lose/draw) to checkers player

--- c/checkers_player.py
+++ c/checkers_player.py
@@ -2,6 +2,7 @@
 
 from bitboard_32_state import Bitboard32State
 from sam_server import SamServer
+from structs import *
 
 from random import randint
 
@@ -9,14 +10,15 @@ moves = []
 
 board = Bitboard32State()
 server = SamServer()
-
-if server.connect(True):
+player = server.connect()
+if player:
     response = server.send_and_receive("")
     if response:
         nextmove = board.move_from_string(response)
         moves.append(nextmove)
         board = board.result(nextmove)
     else:
+        print('------ERROR------')
         server.disconnect()
 
 while server.connected:
@@ -36,8 +38,19 @@ while server.connected:
         moves.append(nextmove)
         board = board.result(nextmove)
     else:
+        print('------ERROR------')
         server.disconnect()
 
-print('\nRESULT')
+print('\nRESULTS:')
 for move in moves:
     print(move)
+result = cast(board.board, POINTER(BOARD))
+if player and result.contents.b or not player and result.contents.w:
+    board = board.result(next(board.actions()))
+    result = cast(board.board, POINTER(BOARD))
+    if not player and result.contents.b or player and result.contents.w:
+        print("DRAW!\n")
+    else:
+        print("LOST!\n")
+else:
+    print("WON!\n")
