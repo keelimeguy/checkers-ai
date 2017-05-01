994685c492cac8091dce23b13ac3d7167b49097f
Keelin Becker-Wheeler <keelin.wheeler@gmail.com>
2017-04-30 22:43:28 -0400

    Added equality check for bitboard_32_state

--- checkers/state/bitboard_32_state.py
+++ checkers/state/bitboard_32_state.py
@@ -76,6 +76,9 @@ class Bitboard32State(CheckersGameState):
     def __del__(self):
         state32_lib.Board_destroy(self.c_board)
 
+    def __eq__(self, other):
+        return self.c_board.w == other.c_board.w and self.c_board.b == other.c_board.b and self.c_board.k == other.c_board.k and self.c_board.plyr == other.c_board.plyr
+
     @classmethod
     def from_string(cls, board_string=FRESH_BOARD_REPR):
         new_board = state32_lib.Board_from_string(create_string_buffer(board_string.encode("utf-8")))
